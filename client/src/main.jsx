ad05daacc2986b645b97798ad9ef298cf48aff26
jeahee2 <leewpgml@gmail.com>
2026-05-28 16:03:30 +0900

    teset

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import './reset.css'
 import TodoListApp from './TodoListApp.jsx'
-import HomeApp from '.HomeApp.jsx'
+// import HomeApp from '.HomeApp.jsx'
 import RouterApp from './RouterApp.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
