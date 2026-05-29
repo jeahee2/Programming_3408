dc4bf70839df2f240fc55399f9a5024a95c144cf
jeahee2 <leewpgml@gmail.com>
2026-05-29 14:20:56 +0900

    correction

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import './reset.css'
 import TodoListApp from './TodoListApp.jsx'
-// import HomeApp from '.HomeApp.jsx'
+import HomeApp from './HomeApp.jsx'
 import RouterApp from './RouterApp.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterApp/>
+    <HomeApp />
   </StrictMode>
 )
